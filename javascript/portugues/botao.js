594ca0c665858f58a329efce0fd9005cbfeca153
Vladimir da Silva Jr <vladimir.sjr@gmail.com>
2023-07-31 19:55:10 -0300

    Separando js em portugues

--- javascript/portugues/botao.js
+++ javascript/portugues/botao.js
@@ -1,44 +1,3 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");   
-        }
-    })
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0})
-    })    
-    var typed = new Typed(".typing", {
-        strings:["Análise e Desenvolvimento de Sistemas"],
-        typeSpeed:100
-    })
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-    })
-});
-
 const btnMobile = document.getElementById('menu-btn');
 function toggleMenu(event){
     if(event.type === 'touchstart'){event.Default()};
@@ -96,27 +55,4 @@ const debounce = function(func, wait, immediate){
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args)
     }
-};
-
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
-
-function animeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
-    target.forEach(function (element){
-        if((windowTop) > element.offsetTop){
-            element.classList.add(animationClass);
-        }
-        else{
-            element.classList.remove(animationClass)
-        }
-    })
-};
-
-animeScroll();
-
-if (target.length){
-    window.addEventListener('scroll', debounce(function(){
-        animeScroll();
-    }, 200));
 };
