0901f8ec04a047061126ad39bee16c4dc82d36e1
Vladimir da Silva Jr <vladimir.sjr@gmail.com>
2023-08-02 22:16:13 -0300

    Traduzindo para espanhol

--- javascript/español/boton.js
+++ javascript/español/boton.js
@@ -1,44 +1,3 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");   
-        }
-    })
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0})
-    })    
-    var typed = new Typed(".typing", {
-        strings:["Analysis and Systems Development"],
-        typeSpeed:100
-    })
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-    })
-});
-
 const btnMobile = document.getElementById('menu-btn');
 function toggleMenu(event){
     if(event.type === 'touchstart'){event.Default()};
@@ -66,9 +25,9 @@ btnMore.addEventListener('click', function(){
     var serv = document.querySelector('.serv-content');
     serv.classList.toggle('active');
     if(serv.classList.contains('active')){
-        return btnMore.textContent = 'Less';
+        return btnMore.textContent = 'Menos';
     } else{
-        btnMore.textContent = 'More'
+        btnMore.textContent = 'Más'
     }
 });
 
@@ -77,9 +36,9 @@ btnMais.addEventListener('click', function(){
     var hab = document.querySelector('.column-right2');
     hab.classList.toggle('active');
     if(hab.classList.contains('active')){
-        return btnMais.textContent = 'Less';
+        return btnMais.textContent = 'Menos';
     } else{
-        btnMais.textContent = 'More'
+        btnMais.textContent = 'Más'
     }
 });
 
@@ -96,27 +55,4 @@ const debounce = function(func, wait, immediate){
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args)
     }
-};
-
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
-
-function animeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
-    target.forEach(function (element){
-        if((windowTop) > element.offsetTop){
-            element.classList.add(animationClass);
-        }
-        else{
-            element.classList.remove(animationClass)
-        }
-    })
-};
-
-animeScroll();
-
-if (target.length){
-    window.addEventListener('scroll', debounce(function(){
-        animeScroll();
-    }, 200));
 };
